dd0abb5c9684236c5ce3a4948edef3db2035a64c
mahinder <mahinder124@gmail.com>
2014-03-03 22:31:06 +0530

    remove data

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ParseUser
 	alias :email :username
 
-	EMAILNOTIFYTEST = ["nitish.verma@trantorinc.com","mahinder.kumar@trantorinc.com","kirandeep.kaur@trantorinc.com","rajat.julka@trantorinc.com"]
+	EMAILNOTIFYTEST = ["nitish.verma@trantorinc.com","mahinder.kumar@trantorinc.com","kirandeep.kaur@trantorinc.com"]
 	EMAILNOTIFYMAIN = ["rajat.julka@trantorinc.com","raj.kondal@trantorinc.com","pradeep@trantorinc.com","sriram@trantorinc.com"]
 
 end

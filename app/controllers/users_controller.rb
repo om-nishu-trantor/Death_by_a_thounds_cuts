d49c515de2f2b67ed4eba54f960f703d11f87923
mahinder <mahinder.kumar@trantorinc.com>
2014-03-26 12:23:20 +0530

    add reset password in continum

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
   load_and_authorize_resource
+  
   def new 
     @users =  current_user.isAdmin ? User.all.map(&:Name) : ['RAJAT JULKA']
     @users.collect! { |c| [ c, c ] } if @users 
@@ -20,4 +21,20 @@ class UsersController < ApplicationController
       render "new"  
     end  
   end
+
+  def edit
+    @users =  current_user.isAdmin ? User.all.map(&:Name) : ['RAJAT JULKA']
+    @users.collect! { |c| [ c, c ] } if @users
+    @user = User.find_by_objectId(params[:id])
+  end
+
+  def update
+    debugger
+    @user = User.find_by_objectId(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to edit_user_path, :notice => "User updated successfully"  
+    else
+      redirect_to edit_user_path, :notice => "Some thing went wrong"
+    end  
+  end  
 end

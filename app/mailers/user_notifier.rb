c1e76901dd7068c11982d201381615fa54b961ce
mahinder <mahinder.kumar@trantorinc.com>
2014-04-14 14:27:10 +0530

    change mail formate

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -5,28 +5,28 @@ class UserNotifier < ActionMailer::Base
   # layout 'email'
 
   def send_create_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut created: "+truncate(object.Description, length: 25)
+    setup_mail object, "DBTC: #{object.Project} cut created: "+truncate(object.title, length: 25)
   end
 
   def send_update_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut updated: "+truncate(object.Description, length: 25)
+    setup_mail object, "DBTC: #{object.Project} cut updated: "+truncate(object.title, length: 25)
   end
 
   def send_delete_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut deleted: "+truncate(object.Description, length: 25)
+    setup_mail object, "DBTC: #{object.Project} cut deleted: "+truncate(object.title, length: 25)
   end
 
   def send_close_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut fixed: "+truncate(object.Description, length: 25)
+    setup_mail object, "DBTC: #{object.Project} cut updated: "+truncate(object.title, length: 25)
   end
 
   def send_mobile_notification_mail header, body
     @body_data = body
-    mail(:to => (User::EMAILNOTIFYTEST).join(',') , :subject => header )
+    mail(:to => (User::EMAILNOTIFYMAIN).join(',') , :subject => header )
   end
   def setup_mail object, sub
     @object_data = object
-    mail(:to => (User::EMAILNOTIFYTEST).join(',') , :subject => sub )
+    mail(:to => (User::EMAILNOTIFYMAIN).join(',') , :subject => sub )
   end  
 
 

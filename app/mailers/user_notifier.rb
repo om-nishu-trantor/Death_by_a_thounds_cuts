41bbac17534a8456d2ee76b3b88ecd5a3e8ef781
mahinder <mahinder.kumar@trantorinc.com>
2014-06-18 10:22:48 +0530

    change in page

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -5,19 +5,19 @@ class UserNotifier < ActionMailer::Base
   # layout 'email'
 
   def send_create_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut created: "+truncate(object.title, length: 25)
+    setup_mail object, "DBTC: #{object.Project} issue created: "+truncate(object.title, length: 25)
   end
 
   def send_update_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut updated: "+truncate(object.title, length: 25)
+    setup_mail object, "DBTC: #{object.Project} issue updated: "+truncate(object.title, length: 25)
   end
 
   def send_delete_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut deleted: "+truncate(object.title, length: 25)
+    setup_mail object, "DBTC: #{object.Project} issue deleted: "+truncate(object.title, length: 25)
   end
 
   def send_close_notification_mail object
-    setup_mail object, "DBTC: #{object.Project} cut updated: "+truncate(object.title, length: 25)
+    setup_mail object, "DBTC: #{object.Project} issue updated: "+truncate(object.title, length: 25)
   end
 
   def send_mobile_notification_mail header, body

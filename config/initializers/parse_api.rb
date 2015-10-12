28a5da5894047b3daa1e1d87b1c9bd05eb1c77d1
Sunia Kalra <sunia.kalra@trantorinc.com>
2015-10-12 17:57:03 +0530

    Set credentials for parse_api

--- config/initializers/parse_api.rb
+++ config/initializers/parse_api.rb
@@ -1,4 +1,4 @@
 require 'parse-ruby-client'
 
-Parse.init :application_id => "Rsf9juWMIBAgNHZcPzYJfpKVpKoa7MRdn4jEreMF",
-           :api_key        => "N2WNbKjkFOPGD6kK54rRsWoqJlIXkp4GJ0l0lUM6"
+Parse.init :application_id => "7OFMk091CR4zL4lCrq8FexHcHyH6PZ52lySoUUuj",
+           :api_key        => "vJtExpXsFcA3noDJ2fTZbamEXp4kgcfMADbHfNlg"

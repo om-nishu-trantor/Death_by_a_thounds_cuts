daaf20494d478df5f6bf631b4a657ed9c01cd7aa
Kapil Handa <kapilhanda07@gmail.com>
2015-10-15 16:10:25 +0530

    Add Rajat and Pradeep Email for Add issue notification on production

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ParseUser
 	validates_presence_of :username, :Name, :password, :email
 	validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	EMAILNOTIFYTEST = ["nitish.verma@trantorinc.com","kapil.handa@trantorinc.com"]
-	EMAILNOTIFYMAIN = ["nitish.verma@trantorinc.com","kapil.handa@trantorinc.com"]
-	# EMAILNOTIFYMAIN = ["rajat.julka@trantorinc.com","raj.kondal@trantorinc.com","pradeep@trantorinc.com","sriram@trantorinc.com"]
+	EMAILNOTIFYMAIN = ["rajat.julka@trantorinc.com","pradeep@trantorinc.com"]
 
 end

38b8b878b2924007a10deb3e84b8cf388b064c47
Sunia Kalra <sunia.kalra@trantorinc.com>
2015-10-29 12:00:31 +0530

    (Import functionality) MOdified the condition to check file format.

--- app/models/issues.rb
+++ app/models/issues.rb
@@ -14,8 +14,6 @@ class Issues < ParseResource::Base
 
   def self.open_spreadsheet(file)
     case File.extname(file.original_filename)
-    when ".csv" then Roo::Csv.new(file.path, packed: nil, file_warning: :ignore)
-    when ".xls" then Roo::Excel.new(file.path, packed: nil, file_warning: :ignore)
     when ".xlsx" then Roo::Excelx.new(file.path, packed: nil, file_warning: :ignore)
     else raise "Unknown file type: #{file.original_filename}"
     end
